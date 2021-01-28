b1d70a8d25da47dc9054e6def28900b2793aaeed
Aahan Singh Charak <aahancharak@gmail.com>
2021-01-28 23:08:28 +0530

    Completed assignment 4

--- Assignment4/turingMachine.py
+++ Assignment4/turingMachine.py
@@ -1,16 +1,18 @@
 #turing machine for a^nb^n-2 n>=2
 symbolSet={'a','b','null','h'}
 inpString=input('Enter the string which you want to test in the turing machine e=>{a,b} : ')
-tape=['null']
+tape=[]
 tape.extend([char for char in inpString]+['null'])
 curState=0
 curTapeIndex=0
 transitions={
-    0:[('null','null','r',0),('h','h','r',0),('a','h','r',1)],
-    1:[('h','h','r',1),('a','a','r',1),('b','h','l',2),('null','null','l',3)],
-    2:[('a','a','l',2),('h','h','l',2),('null','null','r',0)],
-    3:[('a','h','l',4),('h','h','l',3)],
-    4:[('a','a','r',5),('h','h','l',4),('null','null','r','HALT')]
+    0:[('a','x','r',1)],
+    1:[('a','a','r',1),('b','y','l',2),('null','null','l',3),('y','y','r',1)],
+    2:[('a','a','l',2),('y','y','l',2),('x','x','r',0)],
+    3:[('a','a','l',3),('y','y','l',3),('x','x','r',4)],
+    4:[('a','a','r',5)],
+    5:[('y','y','r',5),('null','null','l','HALT')],
+    'HALT':[]
 }
 def MoveLeftOnTape():
     global curTapeIndex
@@ -42,7 +44,7 @@ def performTransitions(symbol):
             break
     return transitionExists
 
-while(curState!=5 and curState!='HALT'):
+while(curState!='HALT'):
     transitionExists=performTransitions(tape[curTapeIndex])
     if not transitionExists:
         print('Transition doesnt exist for {} on {}'.format(tape[curTapeIndex],curState))
@@ -51,6 +53,6 @@ while(curState!=5 and curState!='HALT'):
 
 if curState=='HALT':
     print('String accepted by the turing machine')
-elif curState==5:
+else:
     print('String not accepted by the turing machine')
 
